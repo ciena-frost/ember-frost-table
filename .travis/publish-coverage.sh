cb563270dfafc9350e6023784a0b7e7844bac3cd
Adam Meadows <adam.meadows@gmail.com>
2016-12-15 15:02:47 -0700

    Fix paths before publishing coverage

--- .travis/publish-coverage.sh
+++ .travis/publish-coverage.sh
@@ -6,4 +6,10 @@ then
   exit 0
 fi
 
-bash <(curl -s https://codecov.io/bash) -f coverage/coverage-final.json
+filename="coverage/coverage-final.json"
+
+name=`cat package.json | grep "\"name\":" | awk -F\" '{print $4;}'`
+echo "submitting coverage info for [$name]"
+
+sed -i -e "s/modules\/${name}/addon/g" $filename
+bash <(curl -s https://codecov.io/bash) -f $filename
