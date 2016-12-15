eb2de2d0164ee1fb9ca616c5d28c3081f0c06721
Adam Meadows <adam.meadows@gmail.com>
2016-12-15 12:37:34 -0700

    Add json reporter

--- config/coverage.js
+++ config/coverage.js
@@ -2,6 +2,7 @@ module.exports = {
   coverageEnvVar: 'COVERAGE',
   coverageFolder: 'coverage',
   useBabelInstrumenter: true,
+  reporters: ['json'],
   excludes: [
     '*/app/**/*',
     '*/tests/**/*'
