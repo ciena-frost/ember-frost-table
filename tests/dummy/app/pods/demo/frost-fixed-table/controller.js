068f5f21759427350840f518ecdae5b486860b13
Gabriel Knoy <gabriel.knoy@gmail.com>
2017-01-05 16:09:37 -0800

    Add example dispatch

--- tests/dummy/app/pods/demo/frost-fixed-table/controller.js
+++ tests/dummy/app/pods/demo/frost-fixed-table/controller.js
@@ -1,2 +1,17 @@
 import HeroesController from '../heroes-controller'
-export default HeroesController.extend({})
+
+export default HeroesController.extend({
+  // notifier: inject.service(),
+  actions: {
+    dispatchNotifications (action, row, col, ...args) {
+      // const message = `(${action} ${row} ${col} ${args})`
+      // this.get('notifier').addNotification({
+      //   message: message,
+      //   type: 'success',
+      //   autoClear: true
+      // })
+      console.log('Dispatching: ', action, row, col, args)
+      // This is where you would be
+    }
+  }
+})
